15f003c120331d9b4d9aa3b4bb950966243f937f
ZiJun Yu <zijunyu2018@gmail.com>
2022-08-31 09:00:13 -0500

    stuffies

--- StudyPracticeTest.java
+++ StudyPracticeTest.java
@@ -1,1 +1,17 @@
+import java.lang.reflect.Field;
+import java.util.*;
+import java.io.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+@SuppressWarnings("unchecked")
+public class StudyPracticeTest {
+
+    @Test (timeout=250)
+    public void getProblemTest() throws Exception {
+        StudyPractice p1 = new MultPractice(7, 3);         
+        assertEqauls("nuuuuu....",12, p.)
+
+    }
+
+}
